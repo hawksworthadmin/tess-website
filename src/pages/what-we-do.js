1274ec89b42cbabfad33266f8c6d43cd08a293d9
Emy Ifeora <emyboybeats@gmail.com>
2023-06-14 07:14:15 +0100

    updated meta tags

--- src/pages/what-we-do.js
+++ src/pages/what-we-do.js
@@ -1,10 +1,15 @@
+import METADATA from '@/METADATA'
 import Layout from '@/components/layout/Layout'
 import WhatWeDo from '@/components/pages/WhatWeDo/WhatWeDo'
+import Head from 'next/head'
 import React from 'react'
 
 export default function _WhatWeDo() {
 	return (
 		<Layout>
+			<Head>
+				<title>What We Do | {METADATA.title}</title>
+			</Head>
 			<WhatWeDo />
 		</Layout>
 	)
